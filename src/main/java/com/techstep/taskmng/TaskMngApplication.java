3ef6996c619c05ea6948e3385656ae38669494ba
Zahraahareb <zahraahareb6202@gmail.com>
2026-09-25 23:38:25 +0300

    initial commit

---
+++ src/main/java/com/techstep/taskmng/TaskMngApplication.java
@@ -0,0 +1,13 @@
+package com.techstep.taskmng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMngApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMngApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techstep/taskmng/TaskMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstep.taskmng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMngApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
