1d8f116197c7f6abbd42111b09c72e2f66ce0f51
Zahraahareb <zahraahareb6202@gmail.com>
2026-09-26 00:44:39 +0300

    add task model

---
+++ src/main/java/com/techstep/taskmng/model/Task.java
@@ -0,0 +1,25 @@
+package com.techstep.taskmng.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String description;
+
+    @Column
+    private boolean completed;
+
+}
